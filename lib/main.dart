22592f9e62f7d8ba0949f9eee7c6396062258975
Vlad <garonum@gmail.com>
2021-02-07 22:12:01 +0300

    sections

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,83 @@
 import 'package:flutter/material.dart';
-void main() => runApp(new MyApp());
 
-//final TextEditingController eCtrl = new TextEditingController();
+void main() {
+  runApp(MyApp(
+    items: List<ListItem>.generate(
+      1000,
+          (i) => i % 6 == 0
+          ? HeadingItem("Heading $i")
+          : MessageItem("Sender $i", "Message body $i"),
+    ),
+  ));
+}
+
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext ctxt) {
-    return new MaterialApp(
-      home: new ListDisplay(),
-    );
-  }
-}
-class ListDisplay extends StatefulWidget {
-  @override
-  State createState() => new DyanmicList();
-}
-class DyanmicList extends State<ListDisplay> {
-  List<String> litems = ["1","2","3","4"];
-  final TextEditingController eCtrl = new TextEditingController();
-  @override
-  Widget build (BuildContext ctxt) {
-    return new Scaffold(
-        appBar: new AppBar(title: new Text("Луч Земли"),),
-        body: new Column(
-          children: <Widget>[
-            new TextField(
-              controller: eCtrl,
-              onSubmitted: (text) {
-                litems.add(text);
-                eCtrl.clear();
-                setState(() {});
-              },
-            ),
-            new Expanded(
-                child: new ListView.builder
-                  (
-                    itemCount: litems.length,
-                    itemBuilder: (BuildContext ctxt, int Index) {
-                      return ListTile(
-                        title: Text(litems[Index]),
-                      );
+  final List<ListItem> items;
 
-                    }
-                )
-            )
-          ],
-        )
+  MyApp({Key key, @required this.items}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Mixed List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: ListView.builder(
+          // Let the ListView know how many items it needs to build.
+          itemCount: items.length,
+          // Provide a builder function. This is where the magic happens.
+          // Convert each item into a widget based on the type of item it is.
+          itemBuilder: (context, index) {
+            final item = items[index];
+
+            return ListTile(
+              title: item.buildTitle(context),
+              subtitle: item.buildSubtitle(context),
+            );
+          },
+        ),
+      ),
     );
   }
+}
+
+/// The base class for the different types of items the list can contain.
+abstract class ListItem {
+  /// The title line to show in a list item.
+  Widget buildTitle(BuildContext context);
+
+  /// The subtitle line, if any, to show in a list item.
+  Widget buildSubtitle(BuildContext context);
+}
+
+/// A ListItem that contains data to display a heading.
+class HeadingItem implements ListItem {
+  final String heading;
+
+  HeadingItem(this.heading);
+
+  Widget buildTitle(BuildContext context) {
+    return Text(
+      heading,
+      style: Theme.of(context).textTheme.headline5,
+    );
+  }
+
+  Widget buildSubtitle(BuildContext context) => null;
+}
+
+/// A ListItem that contains data to display a message.
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+
+  MessageItem(this.sender, this.body);
+
+  Widget buildTitle(BuildContext context) => Text(sender);
+
+  Widget buildSubtitle(BuildContext context) => Text(body);
 }
